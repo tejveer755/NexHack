deb98088f8a327eb8f2f42aeeea136751a6f118f
tejveer <tejveersingh.75575@gmail.com>
2025-07-24 01:20:33 +0530

    fix timline component for mobile sceen and remoe light theame

--- components/ui/timeline.jsx
+++ components/ui/timeline.jsx
@@ -28,10 +28,7 @@ export const Timeline = ({ data }) => {
   const opacityTransform = useTransform(scrollYProgress, [0, 0.1], [0, 1]);
 
   return (
-    <div
-      className="w-full font-sans md:px-10 mt-18"
-      ref={containerRef}
-    >
+    <div className="w-full font-sans md:px-10 mt-18  text-white" ref={containerRef}>
       <div className="max-w-7xl mx-auto px-4 md:px-8 lg:px-10">
         <motion.h2
           initial={{ backgroundPosition: '0% 50%' }}
@@ -40,7 +37,7 @@ export const Timeline = ({ data }) => {
           className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4 
           text-transparent bg-clip-text 
           bg-[length:300%_300%] text-center
-          bg-gradient-to-r from-purple-400 via-blue-400 to-teal-300 border-b border-b-zinc-50 w-fit pb-3 mx-auto" 
+          bg-gradient-to-r from-purple-400 via-blue-400 to-teal-300 border-b border-zinc-600 w-fit pb-3 mx-auto" 
         >
           Event Execution Schedule
         </motion.h2>
@@ -53,27 +50,28 @@ export const Timeline = ({ data }) => {
             className="flex justify-start pt-10 md:pt-40 md:gap-32"
           >
             <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
-              <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full dark:bg-black flex items-center justify-center">
-                <div className="h-4 w-4 rounded-full dark:bg-neutral-800 dark:border-neutral-700 p-2 border" />
+              <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-neutral-900 flex items-center justify-center">
+                <div className="h-4 w-4 rounded-full bg-neutral-800 border border-neutral-700 p-2" />
               </div>
-              <h3 className="hidden md:block text-xl md:pl-20 md:text-2xl font-bold dark:text-neutral-50">
+              <h3 className="hidden md:block text-xl md:pl-20 md:text-2xl font-bold text-neutral-200">
                 {item.title} <br /> {item.date}
               </h3>
             </div>
 
             <div className="relative pl-20 pr-4 md:pl-4 w-full">
-              <h3 className="md:hidden block text-2xl mb-4 text-left font-bold dark:text-neutral-500">
-                {item.title}
+              <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-300">
+                {item.title}  <br /> {item.date}
               </h3>
               {item.content}
             </div>
           </div>
         ))}
+
         <div
           style={{
             height: height + "px",
           }}
-          className="absolute md:left-8 left-8 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] dark:via-neutral-700 [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)]"
+          className="absolute md:left-8 left-8 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] via-neutral-700 [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)]"
         >
           <motion.div
             style={{
